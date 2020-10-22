c0370ee6a751672a1967baf0b921483ac1180ef8
Guillermo Rodriguez <guillermo.power@gmail.com>
2020-10-22 01:49:05 -0300

    azure ws
    azure ws

--- main.js
+++ main.js
@@ -1497,7 +1497,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    apiUrl: 'http://localhost:51609'
+    //apiUrl: 'http://localhost:51609'
+    apiUrl: 'http://wsarqcalc.azurewebsites.net'
 };
 /*
  * For easier debugging in development mode, you can import the following file
